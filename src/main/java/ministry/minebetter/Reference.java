211127ace6bb352d3df93009a93f9faa10ecea5b
M1nistry <Ministry@live.com.au>
2016-01-23 01:40:05 +1100

    Initial commit

---
+++ src/main/java/ministry/minebetter/Reference.java
@@ -0,0 +1,7 @@
+package ministry.minebetter;
+
+public class Reference {
+    public static final String MOD_ID = "mb";
+    public static final String MOD_NAME = "MineBetter";
+    public static final String VERSION = "1.0";
+}
